d596dd1e91588660b156b96de3175ace2b11ecf9
Tiffany Marko <tiffanyjmarko@gmail.com>
2022-10-10 12:23:37 -0700

    adding icons

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,12 @@ var responseText = document.getElementById('response-text');
 var city = document.getElementById('city');
 var Wind = document.getElementById('Wind');
 var Humidity = document.getElementById('Humidity');
+var Temperature = document.getElementById('Temperature');
+var Icon = document.getElementById("Icon");
+var fiveday = document.getElementsByClassName("fivedaydata");
 
 function getApi(cityName) {
-  var requestUrl ="https://api.openweathermap.org/data/2.5/weather?q=" +cityName+"&appid=acbbc10a0169525fd8385d8afc492ff3"
+  var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=acbbc10a0169525fd8385d8afc492ff3"
 
   fetch(requestUrl) //the computer copies the URL then "hits enter"
     .then(function (response) {
@@ -19,34 +22,48 @@ function getApi(cityName) {
         responseText.textContent = response.status;
       }
       return response.json();
-  }).then(function (response) {
-    console.log(response)
-    city.textContent = response.name
-    Wind.textContent = "Wind Speed: " + response.wind.speed
-    Humidity.textContent ="Humidity: " + response.main.humidity
+    }).then(function (response) {
+      console.log(response);
+      city.textContent = response.name;
+      Wind.textContent = "Wind Speed: " + response.wind.speed + "MPH";
+      Humidity.textContent = "Humidity: " + response.main.humidity + "%";
+      var temp = 1.8 * (response.main.temp - 273) + 32;
+      Temperature.textContent = "Temperature: " + Math.round(temp) + "°F";
+
+      Icon.src = "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png";
 
 
-    var lat = response.coord.lat
-    var lon = response.coord.lon
 
-    fiveDayForecast(lat, lon)
-  })
-}
 
-function fiveDayForecast(lat,lon) {
-    var request = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=acbbc10a0169525fd8385d8afc492ff3`
+      var lat = response.coord.lat;
+      var lon = response.coord.lon;
 
-    fetch(request).then(function (response) {
-      return response.json()
-    }).then(function (response)  {
-      console.log(response)
-      console.log(response.list[3].main.temp)
+      fiveDayForecast(lat, lon);
     })
 }
 
+function fiveDayForecast(lat, lon) {
+  var request = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=acbbc10a0169525fd8385d8afc492ff3`
+
+  fetch(request).then(function (response) {
+    return response.json();
+  }).then(function (response) {
+    console.log(response);
+    console.log(response.list[3].main.temp);
+    
+
+    for (var i = 0; i < fiveday.length; i++) {
+
+      var temp = 1.8 * (response.list[i].main.temp - 273) + 32;
+      fiveday[i].innerHTML = "<img src='https://openweathermap.org/img/wn/"+response.list[i].weather[0].icon+"@2x.png'><p>Temperature: " + Math.round(temp) + "°F</p><p>Wind: "+response.list[i].wind.speed+"MPH</p><p>Humidity: "+response.list[i].main.humidity+"%</p>";
+
+    }
+  })
+}
 
 
-function search(){
+
+function search() {
   //console.log("search");
   var searchText = document.getElementById('searchbar').value
   //this grabs the searchbar element by ID in the HTML. .value gets the text that is inside the text box
